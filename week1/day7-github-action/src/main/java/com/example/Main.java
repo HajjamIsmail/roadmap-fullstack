b578a7033cdfe1adba4b42dcd1c256fa2280b803
HAJJAM ISMAIL <hajjam.ism@gmail.com>
2025-09-14 14:00:32 +0100

    #ci: test workflow github action1

--- week1/day7-github-action/src/main/java/com/example/Main.java
+++ week1/day7-github-action/src/main/java/com/example/Main.java
@@ -7,6 +7,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello GitHub Actions!");
+        System.out.println("Hello GitHub Actions1!");
+        System.out.println("Hello GitHub Actions2!");
     }
 }
